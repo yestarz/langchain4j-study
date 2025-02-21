e93e90492ee8d1eac533aeba55c7de9fc84f3b4e
yangxin <981987024@qq.com>
2025-02-21 16:05:21 +0800

    first commit

---
+++ 00-project-simple/src/test/java/cn/baruto/langchain4j/ApplicationTest.java
@@ -0,0 +1,7 @@
+package cn.baruto.langchain4j;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+## LangChain4J 学习项目
+
+### 参考文档： https://javaai.pig4cloud.com/
+
+### 视频：https://www.bilibili.com/video/BV19e21YgEgc
